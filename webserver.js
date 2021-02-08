5b8a93546d3ee0a5779a139bdff3fa9209a4804b
Kamil Bagautdinov <kamilb@networkoptix.com>
2021-02-07 23:21:04 -0800

    Adjust webserver.js to handle html/css

--- webserver.js
+++ webserver.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const webserver = express()
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const webserver = express();
+const port = 3000;
+
+webserver.use(express.static(__dirname+'/public'));
 
 webserver.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  // res.send('Hello World')
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write('File Not Found');
+    }
+    else {
+      res.write(data);
+        }
+    res.end();
+    });
+});
 
 webserver.listen(port, () => {
   console.log(`Webserver is running on port: ${port}`)
