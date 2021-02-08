cfd6a72f17552d0adc4366f857b2d12a2f296736
Kamil Bagautdinov <kamilb@networkoptix.com>
2021-02-07 22:26:54 -0800

    Add initial web content

--- test/test_webserver.js
+++ test/test_webserver.js
@@ -3,6 +3,6 @@ const webserver = require('../webserver');
 
 describe('GET /', () => {
   it('Respond with Hello World', (done) =>  {
-    request(webserver).get('/').expect('Hello World', done)
-  })
-})
+    request(webserver).get('/').expect('Hello World', done);
+  });
+});
